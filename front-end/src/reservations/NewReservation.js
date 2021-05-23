fa1eccd5a23c0a06afa961c7d3a28114384e3456
Priscilla <priscillakung@gmail.com>
2021-05-22 22:43:37 -0700

    added validation error for past reservation date and error display

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
+import ErrorAlert from "../layout/ErrorAlert";
 
 function NewReservation() {
     const history = useHistory();
+    const [errors, setErrors] = useState([]);
     const [formData, setFormData] = useState({
         first_name: "",
 	    last_name: "",
 	    mobile_number: "",
 	    reservation_date: "",
 	    reservation_time: "",
-	    people: 0,
+	    people: 1,
     });
 
 function handleChange({ target }) {
@@ -18,21 +20,38 @@ function handleChange({ target }) {
 
 function handleSubmit(event) {
     event.preventDefault();
-    history.push(`/dashboard?date=${formData.reservation_date}`)
+    if (validateDate()) {
+        history.push(`/dashboard?date=${formData.reservation_date}`)
+    }
 }
 
 // Validation function - check if reservation date is Tuesday or in the past
 
 function validateDate() {
     const reserveDate = new Date(formData.reservation_date);
+    const todaysDate = new Date();
     const foundErrors = [];
-    if (reserveDate.getDay() === 2) { // 2 is Tuesday
+    if(reserveDate.getDay() === 2) { // 2 is Tuesday
         foundErrors.push({ message: "Restaurant is closed on Tuesday. Reservations cannot be made."})
     }
+    if(reserveDate < todaysDate) {
+        foundErrors.push({ message: "Reservation cannot be made in the past." });
+    }
+    setErrors(foundErrors);
+    if(foundErrors.length > 0) { // if there are any error messages
+        return false;   // reservation date is not valid
+    }
+    return true; // else, reservation date is valid
+}
+
+const errorsDisplay = () => {
+    return errors.map((error, index) => <ErrorAlert key={index} error={error} />);
 }
 
     return (
         <form>
+            {errorsDisplay()}
+
             <label htmlFor="first_name">First Name:&nbsp;</label>
             <input 
             name="first_name"
