ffd9b4c3105909491536e0569c4d926dbf459446
Priscilla <priscillakung@gmail.com>
2021-06-25 11:49:07 -0700

    getting error Access to fetch at 'https://periodic-tables-backend-two.vercel.app/tables' from origin 'https://periodic-tables-frontend-one.vercel.app' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: The 'Access-Control-Allow-Origin' header has a value 'https://periodic-tables-backend-two.vercel.app' that is not equal to the supplied origin. Have the server send the header with a valid value, or, if an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -12,7 +12,7 @@ const tablesRouter = require("./tables/tables.router");
 
 const app = express();
 
-app.use(cors({origin: `https://periodic-tables-backend-two.vercel.app`}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
